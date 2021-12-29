f21b2f844c4d3da11e081d31c58b00405ebd3ed2
Roshan <90826638+pepesir@users.noreply.github.com>
2021-12-29 19:01:47 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+/* 
+Don't change credits🥲🚶
+Sourcecode https://github.com/pepesir/Bosco
+Want to recode? yes, it's up to you as long as you don't change the watermark
+Don't sell the SC 😐👌
+*/
+
 const {
 WAConnection,
    MessageType,
